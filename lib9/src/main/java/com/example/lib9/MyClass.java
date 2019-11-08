83341a089944129ea011fb09c0186d98188cede2
FrogE04 <fred870126@gmail.com>
2019-11-08 15:13:14 +0800

    Initial commit

---
+++ lib9/src/main/java/com/example/lib9/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib9;
+
+public class MyClass {
+    public static void main(String[] args)
+    {
+        for(int i = 1 ; i< 10 ; i++){
+            for(int j = 1 ; j <10 ; j++)
+                System.out.print(""+i+"X"+j+"="+(i*j)+"\t");
+            System.out.println();
+        }
+    }
+}
